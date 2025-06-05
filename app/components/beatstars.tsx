dcd6a1fc2862367128005a36c958d7d7618d974c
truvor <truvor187@gmail.com>
2025-06-05 11:33:08 -0400

    Add Beatstars tracklist

--- app/components/beatstars.tsx
+++ app/components/beatstars.tsx
@@ -10,9 +10,11 @@ type statsType = {
     following: number
 };
 
+type trackData = { v2Id: string };
+
 const Beatstars = () => {
     const [stats, setStats] = useState<statsType>();
-    const [tracklistIds, setTracklistIds] = useState<string>();
+    const [tracklistIds, setTracklistIds] = useState<Array<trackData>>();
     const [error, setError] = useState<string>();
 
     useEffect(() => {
@@ -33,23 +35,50 @@ const Beatstars = () => {
                 console.error('Error fetching stats:', error);
                 setError('Error fetching stats');
             });
-/*        fetch('https://core.prod.beatstars.net/graphql?op=getProfileContentTrackList')
-            .then((response) => {
+
+        const query = `
+  query getProfileContentTrackList($memberId: String!, $page: Int, $size: Int) {
+    profileTracks(memberId: $memberId, page: $page, size: $size) {
+      content {
+        v2Id
+      }
+    }
+  }
+`;
+
+        const body = {
+            operationName: 'getProfileContentTrackList',
+            query,
+            variables: {
+                memberId: 'MR141266',
+                page: 0,
+                size: 12,
+            }};
+
+        fetch('https://core.prod.beatstars.net/graphql?op=getProfileContentTrackList', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body),
+        })
+            .then(response => {
                 if (!response.ok) {
                     throw new Error('Failed to fetch tracklist');
                 }
-                return response.json();
-            })
-            .then((data) => {
-                if (!data || !data.response || !Array.isArray(data.response.data.content)) {
+                return response.json()}
+            )
+            .then(data => {
+                if (!data || !data.data ) {
                     throw new Error('Failed to fetch tracklist');
                 }
-                setTracklistIds(data.response.data.content);
+
+                setTracklistIds(data.data.profileTracks.content);
             })
-            .catch((error) => {
+            .catch(error => {
                 console.error('Error fetching tracklist:', error);
                 setError('Error fetching tracklist');
-            });*/
+            });
     }, []);
 
     return (<>
@@ -60,9 +89,18 @@ const Beatstars = () => {
                 <span>Beatstars Stats:</span>
                 <span>Followers: {stats.followers}</span>
                 <span>Plays: {stats.plays}</span>
-
-                <span>Tracks: {tracklistIds}</span>
             </p>
+        ) : null}
+
+        {tracklistIds ? (
+            tracklistIds.map(track => {
+                return (
+                    <iframe key={track.v2Id}
+                            src={`//www.beatstars.com/embed/track/?id=${track.v2Id}`}
+                            width='100%' height='140'
+                            style={{border: 'none'}}></iframe>
+                );
+            })
         ) : null}
     </>);
 };
