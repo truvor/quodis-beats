ba61f850e6879716b85c530c0d9bbac5a04b6737
truvor <truvor187@gmail.com>
2025-07-15 16:51:53 -0400

    Integrate Sentry for monitoring errors and performance

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://7819f1b9212d4c95542d6888fa43c61e@o4509669944066048.ingest.de.sentry.io/4509669947801680",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
