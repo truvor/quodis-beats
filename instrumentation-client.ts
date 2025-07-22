8d8e01359e88f45f51f8b5730fb51cafce74bde4
truvor <truvor187@gmail.com>
2025-07-22 14:42:44 -0400

    Revert Sentry to function in all environments

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7819f1b9212d4c95542d6888fa43c61e@o4509669944066048.ingest.de.sentry.io/4509669947801680",
+  dsn: process.env.SENTRY_DSN,
 
   // Add optional integrations for additional features
   integrations: [
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,12 +1,7 @@
-export {};
+import * as Sentry from "@sentry/nextjs";
 
-if (process.env.NODE_ENV === "production") {
-  import("@sentry/nextjs").then(Sentry => {
-    Sentry.init({
-      dsn: "https://7819f1b9212d4c95542d6888fa43c61e@o4509669944066048.ingest.de.sentry.io/4509669947801680",
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
 
-      tracesSampleRate: 1.0
-    });
-  });
-}
-
+  tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.2 : 1.0
+});
