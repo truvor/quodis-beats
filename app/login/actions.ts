bde5ca293b18a3fb859a0c7ba044fd6931bc9a86
truvor <truvor187@gmail.com>
2025-10-08 21:48:47 -0400

    Add login and sign up via Supabase

---
+++ app/login/actions.ts
@@ -0,0 +1,47 @@
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+import { createClient } from '@/utils/supabase/server'
+
+export async function login(formData: FormData) {
+  'use server'
+  const supabase = await createClient()
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+
+  const { error } = await supabase.auth.signInWithPassword(data)
+
+  if (error) {
+    redirect('/error')
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/')
+}
+
+export async function signup(formData: FormData) {
+  'use server'
+  const supabase = await createClient()
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+
+  const { error } = await supabase.auth.signUp(data)
+
+  if (error) {
+    throw new Error(error.message)
+
+    //redirect('/error')
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/')
+}
