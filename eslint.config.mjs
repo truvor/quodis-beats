ba96d2ab38bb7aed0a5c8345cd8213a697762928
truvor <truvor187@gmail.com>
2025-10-20 15:34:19 -0400

    Switch next lint to ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,8 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends("next/core-web-vitals", "next/typescript"), {
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}];
 
 export default eslintConfig;
