110691ce99c28193827aa6d7093eb0716260fdee
truvor <truvor187@gmail.com>
2025-12-15 20:08:46 -0500

    Utilize Vercel Blob

--- app/components/player-wrapper/player-wrapper.tsx
+++ app/components/player-wrapper/player-wrapper.tsx
@@ -4,7 +4,12 @@ import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
 
 function PlayerWrapper() {
-  return <AudioPlayer src="/audio/foo.mp3" showSkipControls />;
+  return (
+    <AudioPlayer
+      src="https://9ty5evfxd16kbu1f.public.blob.vercel-storage.com/GANG-DNjLaVlzP0QVzWmDeikbOd8LaAfNmB.mp3"
+      showSkipControls
+    />
+  );
 }
 
 export default PlayerWrapper;
