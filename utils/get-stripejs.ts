7605e452c1ae8d32e495af623a3b1ec98522366a
truvor <truvor187@gmail.com>
2025-12-19 17:54:05 -0500

    Fix a Sonar medium issue

--- utils/get-stripejs.ts
+++ utils/get-stripejs.ts
@@ -3,7 +3,7 @@ import { Stripe, loadStripe } from "@stripe/stripe-js";
 let stripePromise: Promise<Stripe | null>;
 
 export default function getStripe(): Promise<Stripe | null> {
-  if (!stripePromise)
+  if (stripePromise !== undefined)
     stripePromise = loadStripe(
       process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string,
     );
