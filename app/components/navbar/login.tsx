b08a517c4a452a510e9593783d1cbda333bb2a6f
truvor <truvor187@gmail.com>
2026-02-13 21:13:18 -0500

    Add sign out and brush up auth

--- app/components/navbar/login.tsx
+++ app/components/navbar/login.tsx
@@ -1,8 +1,42 @@
-import React from "react";
-import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { createClient } from "@/utils/supabase/client";
 
-export default function Login({handleClick}: {handleClick: ()=> void}) {
-  return (<li><Link onClick={handleClick}
-                    href='/admin'><span>Login</span></Link></li>
+export default function Login({ handleClick }: { handleClick: () => void }) {
+  const supabase = createClient();
+  const [label, setLabel] = useState("Login\u00A0\u00A0");
+  const [loggedIn, setLoggedIn] = useState<boolean | undefined>();
+  const router = useRouter();
+
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      const newVal = session?.user ? true : undefined;
+      setLoggedIn(newVal);
+      setLabel(newVal ? "Logout" : "Login\u00A0\u00A0");
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, []);
+
+  const toggleLogin = async () => {
+    handleClick();
+    if (loggedIn) {
+      await supabase.auth.signOut();
+      setLoggedIn(undefined);
+      setLabel("Login\u00A0\u00A0");
+    }
+    router.push("/login");
+  };
+
+  return (
+    <li>
+      <button onClick={toggleLogin}>
+        <span>{label}</span>
+      </button>
+    </li>
   );
 }
