a50141da9f75333d070c6adc3ef063085cd8e913
truvor <truvor187@gmail.com>
2026-02-17 14:53:13 -0500

    Show a set of beat cards

--- app/components/player-wrapper/player-wrapper.tsx
+++ app/components/player-wrapper/player-wrapper.tsx
@@ -1,23 +1,39 @@
 "use client";
 
-import Link from "next/link";
+import { useRef, useState } from "react";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
+import BeatCard from "./beat-card";
 
 function PlayerWrapper() {
+  const [beatUrl, setBeatUrl] = useState(
+    "https://9ty5evfxd16kbu1f.public.blob.vercel-storage.com/GANG-DNjLaVlzP0QVzWmDeikbOd8LaAfNmB.mp3",
+  );
+  const [isPlaying, setIsPlaying] = useState(false);
+  const player = useRef(null);
+
+  const togglePlay = (url: string) => {
+    setBeatUrl(
+      "https://9ty5evfxd16kbu1f.public.blob.vercel-storage.com/GOOD.mp3",
+    );
+    setIsPlaying(!isPlaying);
+    player?.current?.playAudioPromise();
+  };
+
   return (
-    <>
+    <div className="flex flex-col items-center">
+      {Array(10)
+        .fill(null)
+        .map((_item, index) => (
+          <BeatCard key={index} play={togglePlay} />
+        ))}
       <AudioPlayer
-        src="https://9ty5evfxd16kbu1f.public.blob.vercel-storage.com/GANG-DNjLaVlzP0QVzWmDeikbOd8LaAfNmB.mp3"
+        className="sticky bottom-0"
+        src={beatUrl}
         showSkipControls
+        ref={player}
       />
-      <Link
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        href="/admin/purchase"
-      >
-        Buy
-      </Link>
-    </>
+    </div>
   );
 }
 
