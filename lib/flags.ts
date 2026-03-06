6e4d74f1e29ad20b72a5c56919a170d240995311
truvor <truvor187@gmail.com>
2026-03-06 14:52:19 -0500

    Show Player only for authorized user

--- lib/flags.ts
+++ lib/flags.ts
@@ -1,7 +1,28 @@
-import { flag } from 'flags/next';
+import { flag, dedupe } from 'flags/next';
 import { vercelAdapter } from '@flags-sdk/vercel';
+import { createClient } from "@/utils/supabase/server";
+
+type Entity = {
+    user?: {
+        id: string;
+    };
+};
+
+const identify = dedupe(async (): Promise<Entity> => {
+    const supabase = await createClient();
+    const { data: { session } = {} } = await supabase.auth.getSession();
+
+    return {
+        user: session?.user
+            ? {
+                id: session.user.id
+            }
+            : undefined
+    };
+});
 
 export const adminFlag = flag({
     key: "admin",
+    identify,
     adapter: vercelAdapter()
 });
