3bd0ec9344ddda355bc16ad023c9f513293c4479
truvor <truvor187@gmail.com>
2026-03-06 12:28:49 -0500

    Force output flag value

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,11 @@
 import { adminFlag } from "@/lib/flags";
 import LoginForm from "./login-form";
+import { useEffect } from "react";
 
 export default async function LoginPage() {
   const isAdminFlagEnabled = (await adminFlag()) as boolean;
-  console.log("isAdminFlagEnabled", isAdminFlagEnabled);
+  useEffect(() => {
+    console.log("isAdminFlagEnabled", isAdminFlagEnabled);
+  }, [isAdminFlagEnabled]);
   return <LoginForm isAdminFlagEnabled={isAdminFlagEnabled} />;
 }
